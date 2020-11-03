c1dc11a3464b88a7809d761e74f4b64a0c7b00eb
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-11-04 00:36:31 +0100

    Manage git config and ignores using home-manager

--- home.nix
+++ home.nix
@@ -73,4 +73,39 @@ in
     '';
   };
 
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "Sebastian Nagel";
+    userEmail = "sebastian.nagel@ncoding.at";
+    aliases = {
+      co = "checkout";
+      graph = "log --graph --oneline --decorate";
+      changelog = "log --pretty=format:'%s' --no-merges";
+      count = "shortlog -sn";
+      wtf = "!$DOTFILES/bin/git-wtf";
+      up = "!$DOTFILES/bin/git-up";
+      dlm = "!$DOTFILES/bin/git-delete-local-merged";
+      sup = "!$DOTFILES/bin/git-sup";
+    };
+    ignores = [
+      "*~"
+      "*.swp"
+      ".clang_complete"
+      "TAGS"
+      "dist*/"
+    ];
+    extraConfig = {
+      color.ui = true;
+      diff.submodule = "log";
+      fetch.recurseSubmodules = "on-demand";
+      pull.rebase = "preserve";
+      push.default = "simple";
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      status.submoduleSummary = true;
+    };
+  };
 }
