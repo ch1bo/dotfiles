a4950741d5985dd3743917b5b6ce045c8de143b4
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-11-04 18:52:23 +0100

    Move some env variable and path initialization

--- home.nix
+++ home.nix
@@ -1,10 +1,19 @@
 { config, pkgs, ... }:
+let
+  dotFilesDirectory = "$HOME/.dotfiles";
+in
 {
   programs.home-manager.enable = true;
+  home.stateVersion = "20.09";
   home.username = "ch1bo";
   home.homeDirectory = "/home/ch1bo";
-  home.stateVersion = "20.09";
-
+  home.sessionVariables = {
+    DOTFILES = dotFilesDirectory;
+    EDITOR = "emacs";
+  };
+  home.sessionPath = [
+    "${dotFilesDirectory}/bin"
+  ];
   imports = [
     ./git
     ./nix
