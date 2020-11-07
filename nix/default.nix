9d6fd46176fed453f78b95c8051a79e8750def4d
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-11-07 15:46:15 +0100

    Move LOCALE_ARCHIVE workaround to nix/default.nix

--- nix/default.nix
+++ nix/default.nix
@@ -8,4 +8,11 @@
   # Use default paths instead of config.nixpkgs for nix-env etc.
   xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
   xdg.configFile."nixpkgs/overlays.nix".source = ./nixpkgs-overlays.nix;
+
+  # Provide a compatible locale-archive to nix applications using
+  # LOCALE_ARCHIVE. This avoids problems where the host's glibc is newer and
+  # locales are incompatible.
+  home.sessionVariables = {
+    LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  };
 }
