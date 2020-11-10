f38d8b92c5975e00a0475ffd527c7f2717a87b22
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-11-10 08:56:52 +0100

    Run 'doom sync' on doom.d file changes

--- emacs/default.nix
+++ emacs/default.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 let
-  # TODO how to avoid this hardcoded path?
+  # TODO how to avoid this hardcoded path here and also in emacs configs?
   dotfilesPath = "$HOME/.dotfiles";
 in
 {
@@ -14,5 +14,9 @@ in
   '';
   # TODO replace .dotfiles/bin path for doom, doom-emacs and spacemacs?
   home.file.".spacemacs".source = ./spacemacs;
-  home.file.".doom.d".source = ./doom.d;
+  home.file.".doom.d" = {
+    source = ./doom.d;
+    # TODO how to also perform this when .dotfiles/emacs/doom.d changes?
+    onChange = "doom sync";
+  };
 }
