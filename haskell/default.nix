892aa94988b6525f8c5c3bd17ad7dd8bbd998d25
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-11-19 10:02:00 +0100

    [haskell] Extend sessionPath

--- haskell/default.nix
+++ haskell/default.nix
@@ -1,3 +1,10 @@
+{ config, ... }:
 {
   home.file.".ghc/ghci.conf".source = ./ghci.conf;
+
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.local/bin" # stack install path
+    "${config.home.homeDirectory}/.cabal/bin" # cabal install path
+    "${config.home.homeDirectory}/.ghcup/bin" # ghcup install path
+  ];
 }
