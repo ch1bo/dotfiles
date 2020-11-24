eb019257c8d8456abd6fb8226a33f96a769cc8ed
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-11-24 22:08:15 +0100

    [haskell] Add haskell-language-server

--- haskell/default.nix
+++ haskell/default.nix
@@ -1,10 +1,17 @@
 { config, ... }:
+# TODO: use a flake.nix inputs instead
+let
+  unstable = import <unstable> { };
+in
 {
   home.file.".ghc/ghci.conf".source = ./ghci.conf;
 
   home.sessionPath = [
     "${config.home.homeDirectory}/.local/bin" # stack install path
     "${config.home.homeDirectory}/.cabal/bin" # cabal install path
-    "${config.home.homeDirectory}/.ghcup/bin" # ghcup install path
+  ];
+
+  home.packages = [
+    unstable.haskell-language-server
   ];
 }
