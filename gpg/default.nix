667d9fd39410018a4eab87591cd8a356562c29f1
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-02-08 21:09:42 +0100

    [home] Add mailvelope support to firefox

--- gpg/default.nix
+++ gpg/default.nix
@@ -5,6 +5,8 @@
   # Hardened configuration from github.com/drduh/config
   home.file.".gnupg/gpg.conf".source = ./gpg.conf;
 
+  # TODO: GPG agent replicates ssh keys from keepassxc into
+  # .gnupg/private-keys-v1.d
   services.gpg-agent = {
     enable = true;
     # TODO: This is incompatible with agents = [ "ssh" ] of keychain
@@ -20,6 +22,17 @@
     keys = [ ];
   };
 
-  # TODO: GPG agent replicates ssh keys from keepassxc into
-  # .gnupg/private-keys-v1.d
+  # Mainly for mailvelope
+  home.packages = [ pkgs.gpgme ];
+  home.file.".mozilla/native-messaging-hosts/gpgmejson.json".text = ''
+    {
+      "name": "gpgmejson",
+      "description": "Integration with GnuPG",
+      "path": "${pkgs.gpgme.dev}/bin/gpgme-json",
+      "type": "stdio",
+      "allowed_extensions": [
+        "jid1-AQqSMBYb0a8ADg@jetpack"
+      ]
+    }
+  '';
 }
