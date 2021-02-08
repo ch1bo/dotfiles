21739235080aa5aafc808769bf80f2c4dcf378d4
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-02-08 16:02:56 +0100

    [home] Use gpg-agent for SSH

--- gpg/default.nix
+++ gpg/default.nix
@@ -5,9 +5,18 @@
   # Hardened configuration from github.com/drduh/config
   home.file.".gnupg/gpg.conf".source = ./gpg.conf;
 
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  };
+
   # Manage SSH and GPG agents
   programs.keychain = {
     enable = true;
-    keys = [ ]; # Added by hand or via keepassxc
+    # NOTE: Disable SSH agent as we use the GPG agent for that
+    # TODO: "ssh" here is incompatible with gpg-agent.enableSshSupport
+    agents = [ "gpg" ];
+    # Other keys are added by hand or via keepassxc
+    keys = [ "0x59FAA903C906659A" ];
   };
 }
