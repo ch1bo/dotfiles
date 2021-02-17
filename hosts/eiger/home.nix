64d65662d2822c5d4fb8cd66b1b784aa4c985d4e
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-02-17 07:50:50 +0100

    [matterhorn] Prepare for NixOS installation

--- hosts/eiger/home.nix
+++ hosts/eiger/home.nix
@@ -6,7 +6,7 @@
 
   home.username = "ch1bo";
   # TODO(SN): make "wifi" optional
-  wifi = "wlp0s20f3";
+  wifi = "wlp0s0000";
 
   # Use setuid-wrapped slock
   services.screen-locker.lockCmd = "/run/wrappers/bin/slock";
