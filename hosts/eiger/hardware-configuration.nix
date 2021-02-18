9c7dc4d55bd1927a830c6f23a6e07a6274b6ae6c
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-02-18 08:50:31 +0100

    [eiger] Use 'ondemand' governor

--- hosts/eiger/hardware-configuration.nix
+++ hosts/eiger/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
@@ -14,23 +15,25 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/646b5d5a-06ce-4110-a4bd-0c19c7060294";
+    {
+      device = "/dev/disk/by-uuid/646b5d5a-06ce-4110-a4bd-0c19c7060294";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7BDE-95DA";
+    {
+      device = "/dev/disk/by-uuid/7BDE-95DA";
       fsType = "vfat";
     };
 
   fileSystems."/data" =
-    { device = "/dev/disk/by-uuid/1d61d2c1-d635-401e-b87d-09fdcb8fbe3f";
+    {
+      device = "/dev/disk/by-uuid/1d61d2c1-d635-401e-b87d-09fdcb8fbe3f";
       fsType = "ext4";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/409bc5b6-6379-4e21-897c-c9e2ecf365ce"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/409bc5b6-6379-4e21-897c-c9e2ecf365ce"; }];
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = "ondemand";
 }
