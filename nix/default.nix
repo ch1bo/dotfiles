af60647833ca99b9b6175ebdadfb617be476edb8
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-05-23 09:44:34 +0200

    Override direnv version used in home-manager

--- nix/default.nix
+++ nix/default.nix
@@ -1,10 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  # TODO use nix flakes
+  unstable = import
+    (builtins.fetchTarball {
+      # Descriptive name to make the store path easier to identify
+      name = "nixpkgs-unstable-2022-04-25";
+      url = "https://github.com/nixos/nixpkgs/archive/6a323903ad07de6680169bb0423c5cea9db41d82.tar.gz";
+      sha256 = "0mz0kamp3k8py6hswpnrv1fm3jav48m76pzaw6r7ic2r837bnpw9";
+    })
+    { };
+in
+
 {
 
-  # Use config files instead of config.nixpkgs module for nix-env etc.
+  # Also use config files for nix-env etc.
   nixpkgs.config = import ./nixpkgs-config.nix;
   xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
+  nixpkgs.overlays = import ./nixpkgs-overlays.nix ++ [
+    (self: super: {
+      direnv = unstable.direnv;
+      nix-direnv = unstable.nix-direnv;
+    })
+  ];
   xdg.configFile."nixpkgs/overlays.nix".source = ./nixpkgs-overlays.nix;
 
   # TODO nix.conf holds substituters and should be writable by e.g. 'cachix use'
