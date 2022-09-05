19f465bf5964f1d9f118df6b84b1ed23890cca76
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-09-05 16:30:43 +0200

    Use IOHK substituters

--- hosts/liskamm/hydraw.nix
+++ hosts/liskamm/hydraw.nix
@@ -10,7 +10,18 @@ let
       sha256 = "020fwimsm24yblr1fmnwx240wj8r3x715p89cpjgnnd8axwf32p0";
     })
     { };
-in {
+in
+{
+  # Add iohk substituters
+  nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://hydra.iohk.io"
+  ];
+  nix.settings.trusted-public-keys = [
+    "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+
   # services.nginx.virtualHosts."hydraw.fk.ncoding.at" = {
   #   forceSSL = true;
   #   enableACME = true;
