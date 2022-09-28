f38c138c341ef5c7a4e4de99bf45929e98695fcb
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-09-28 16:25:54 +0200

    A new ip for franco (again)

--- hosts/liskamm/hydraw.nix
+++ hosts/liskamm/hydraw.nix
@@ -80,7 +80,7 @@ in
         [ "--peer" "13.38.49.252:5001" ] # sasha
         [ "--cardano-verification-key" "/data/credentials/sasha.cardano.vk" ]
         [ "--hydra-verification-key" "/data/credentials/sasha.hydra.vk" ]
-        [ "--peer" "35.181.33.41:5001" ] # franco
+        [ "--peer" "52.47.155.199:5001" ] # franco
         [ "--cardano-verification-key" "/data/credentials/franco.cardano.vk" ]
         [ "--hydra-verification-key" "/data/credentials/franco.hydra.vk" ]
       ];
