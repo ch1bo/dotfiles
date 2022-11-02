851badbb1f3b72f851d37d775668cf781b1cae3f
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-11-02 12:42:45 +0100

    [hydraw] Changed path of genesis file

--- hosts/liskamm/hydraw.nix
+++ hosts/liskamm/hydraw.nix
@@ -79,7 +79,7 @@ in
         [ "--hydra-scripts-tx-id" hydraScriptsTxId ]
         [ "--hydra-signing-key" "/credentials/sebastian.hydra.sk" ]
         [ "--cardano-signing-key" "/credentials/sebastian.cardano.sk" ]
-        [ "--ledger-genesis" "/cardano-node/config/preview/shelley-genesis.json" ]
+        [ "--ledger-genesis" "/cardano-node/config/preview/genesis/shelley.json" ]
         [ "--ledger-protocol-parameters" "/data/protocol-parameters.json" ]
         [ "--network-id" networkMagic ]
         [ "--node-socket" "/cardano-node/node.socket" ]
