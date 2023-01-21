afe2333ff88783c2953cffc45d9d2f075897bd3f
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-01-21 17:13:55 +0100

    [liskamm] Nextcloud backup service

--- hosts/liskamm/nextcloud.nix
+++ hosts/liskamm/nextcloud.nix
@@ -77,4 +77,28 @@ in
     };
   };
 
+  systemd.timers."backup-nextcloud" = {
+    wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+        Unit = "backup-nextcloud.service";
+      };
+  };
+
+  systemd.services."backup-nextcloud" = {
+    script = ''
+      set -eu
+      # Backup
+      ${pkgs.gnutar}/bin/tar -czf /backup/db-$(date -I).tar.gz -C /data/db .
+      ${pkgs.gnutar}/bin/tar -czf /backup/nexctloud-$(date -I).tar.gz -C /data/nextcloud apps/ config/
+
+      # Rotate backups, keep 7 days
+      ${pkgs.findutils}/bin/find /backup/ -maxdepth 1 -mtime +7 -delete;
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "nobody";
+    };
+  };
 }
