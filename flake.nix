d9bdf413d9a1226b908496f1fff3c158d6a5565d
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-02-25 14:13:28 +0100

    Add nix-community cachix for emacs-overlay cache hits

--- flake.nix
+++ flake.nix
@@ -20,10 +20,12 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://nix-community.cachix.org"
       "https://cache.iog.io"
       "https://hydra-node.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "hydra-node.cachix.org-1:vK4mOEQDQKl9FTbq76NjOuNaRD4pZLxi1yri31HHmIw="
     ];
