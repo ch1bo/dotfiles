d796f02e11da0d8154157ffcf7f569cd69cf331b
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-04-01 12:35:32 +0200

    [liskamm] Backup all nextcloud files

--- hosts/liskamm/backup.nix
+++ hosts/liskamm/backup.nix
@@ -6,9 +6,22 @@
       repo = "zp1865b5@zp1865b5.repo.borgbase.com:repo";
       encryption = {
         mode = "repokey-blake2";
-        passCommand = "cat /run/keys/borg/pictures_passphrase";
+        passCommand = "cat /run/keys/borg/pictures.pass";
       };
-      environment = { BORG_RSH = "ssh -i /run/keys/borg/id_ed25519_repo_pictures"; };
+      environment = { BORG_RSH = "ssh -i /run/keys/borg/id_ed25519"; };
+      compression = "auto,lzma";
+      startAt = "daily";
+    };
+
+    nextcloud-ch1bo = {
+      paths = [ "/data/nextcloud/data/ch1bo/files" ];
+      doInit = true;
+      repo = "n7ixpw3b@n7ixpw3b.repo.borgbase.com:repo";
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "cat /run/keys/borg/nextcloud-ch1bo.pass";
+      };
+      environment = { BORG_RSH = "ssh -i /run/keys/borg/id_ed25519"; };
       compression = "auto,lzma";
       startAt = "daily";
     };
