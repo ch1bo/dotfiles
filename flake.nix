639ed19d3871de8ff0759ad5e914e8dcecc04086
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-05-10 13:04:32 +0200

    [hydraw] Update hydra sources

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     emacsOverlay.url = "github:nix-community/emacs-overlay";
 
     # hydraw
-    hydra.url = "github:input-output-hk/hydra";
+    hydra.url = "github:input-output-hk/hydra/5642332bef2085974cf6936a47b2a406caba45d9";
     # The workbench flake inputs have some broken recursion in cardano-node
     # https://github.com/input-output-hk/cardano-node/pull/4865
     cardano-node = {
@@ -33,14 +33,16 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.iog.io"
-      "https://hydra-node.cachix.org"
       "https://cache.zw3rk.com"
+      "https://hydra-node.cachix.org"
+      "https://cardano-scaling.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "hydra-node.cachix.org-1:vK4mOEQDQKl9FTbq76NjOuNaRD4pZLxi1yri31HHmIw="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+      "hydra-node.cachix.org-1:vK4mOEQDQKl9FTbq76NjOuNaRD4pZLxi1yri31HHmIw="
+      "cardano-scaling.cachix.org-1:RKvHKhGs/b6CBDqzKbDk0Rv6sod2kPSXLwPzcUQg9lY="
     ];
     allow-import-from-derivation = true;
   };
