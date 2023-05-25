af2265457e7f8bc43815c4cf577197f7311aab1c
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-05-25 07:53:27 +0200

    Before update to 23.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
