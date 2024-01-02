7d8fb4b14d357dcbb670a903ddb0b5d4a8dead19
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-01-02 22:49:59 +0100

    [liskamm] Prune borg backups

--- hosts/liskamm/backup.nix
+++ hosts/liskamm/backup.nix
@@ -11,6 +11,11 @@
       environment = { BORG_RSH = "ssh -i /run/keys/borg/id_ed25519"; };
       compression = "auto,lzma";
       startAt = "daily";
+      prune.keep = {
+        daily = 7; # Keep 7 daily archives
+        weekly = 4; # Keep 4 weekly archives
+        monthly = -1; # Keep at least one archive for each month
+      };
     };
 
     nextcloud-veronika = {
@@ -24,6 +29,11 @@
       environment = { BORG_RSH = "ssh -i /run/keys/borg/id_ed25519"; };
       compression = "auto,lzma";
       startAt = "daily";
+      prune.keep = {
+        daily = 7; # Keep 7 daily archives
+        weekly = 4; # Keep 4 weekly archives
+        monthly = -1; # Keep at least one archive for each month
+      };
     };
   };
 }
