45d91900b0877f615a1065ce8161e09e7a826615
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-03-10 18:38:12 +0100

    [liskamm] Off-site backup of mail

--- hosts/liskamm/backup.nix
+++ hosts/liskamm/backup.nix
@@ -1,5 +1,23 @@
 {
   services.borgbackup.jobs = {
+    mail = {
+      paths = [ "/data/mail" ];
+      doInit = true;
+      repo = "smsua417@smsua417.repo.borgbase.com:repo";
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "cat /run/keys/borg/mail.pass";
+      };
+      environment = { BORG_RSH = "ssh -i /run/keys/borg/id_ed25519"; };
+      compression = "auto,lzma";
+      startAt = "daily";
+      prune.keep = {
+        daily = 7; # Keep 7 daily archives
+        weekly = 4; # Keep 4 weekly archives
+        monthly = -1; # Keep at least one archive for each month
+      };
+    };
+
     nextcloud-ch1bo = {
       paths = [ "/data/nextcloud/data/ch1bo/files" ];
       doInit = true;
