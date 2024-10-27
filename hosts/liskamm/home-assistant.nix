1b41c06d88881f91239a3f97ac4e81fb920a6ed6
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-10-27 11:53:15 +0100

    [liskamm] Back up home-assistant directory

--- hosts/liskamm/home-assistant.nix
+++ hosts/liskamm/home-assistant.nix
@@ -7,9 +7,6 @@ let
   port = 8123;
 in
 {
-  # XXX: really expose port of only proxy it?
-  networking.firewall.allowedTCPPorts = [ port ];
-
   services.nginx.virtualHosts."home.ncoding.at" = {
     forceSSL = true;
     enableACME = true;
@@ -23,14 +20,30 @@ in
 
   virtualisation.oci-containers.containers.home-assistant = {
     image = "ghcr.io/home-assistant/home-assistant:2024.5.0b1";
-    volumes = [
-      # TODO: Backup config + databases
-      "/data/home-assistant:/config"
-    ];
+    volumes = [ "/data/home-assistant:/config" ];
     environment.TZ = "Europe/Vienna";
     extraOptions = [
       "--network=host"
       "--device=/dev/ttyUSB0:/dev/ttyUSB0"
     ];
+  };
+
+  # Backup to borgbase
+  services.borgbackup.jobs.home-assistant = {
+    paths = [ "/data/home-assistant" ];
+    doInit = true;
+    repo = "n8j084i7@n8j084i7.repo.borgbase.com:repo";
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /root/keys/borg/home-assistant.pass";
+    };
+    environment.BORG_RSH = "ssh -i /root/keys/borg/id_ed25519";
+    compression = "auto,lzma";
+    startAt = "daily";
+    prune.keep = {
+      daily = 7; # Keep 7 daily archives
+      weekly = 4; # Keep 4 weekly archives
+      monthly = 12; # Keep 12 monthly archives
+    };
   };
 }
