ee842f46acc5802485e8fe5c931b6dd947fbf81f
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-11-17 11:07:44 +0100

    [liskamm] Add webmail and close port 587

--- hosts/liskamm/mailserver.nix
+++ hosts/liskamm/mailserver.nix
@@ -6,55 +6,80 @@
 # Documentation of environment variables
 # https://docker-mailserver.github.io/docker-mailserver/latest/config/environment/
 #
+# Also includes a proxied roundcube webmail client.
+#
 # Off-site backups using borgbase.
 { config, pkgs, lib, ... }:
 let
   version = "14.0.0";
   serverName = "mail.ncoding.at";
+  webmailPort = 8000; # not exposed
+  imapPort = 993;
 in
 {
+  # Exposes the mail server ports
+  networking.firewall.allowedTCPPorts = [ 25 465 993 ];
+
+  # Exposes the webmail client
   services.nginx.virtualHosts.${serverName} = {
     forceSSL = true;
     enableACME = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString webmailPort}";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+    };
   };
 
-  networking.firewall.allowedTCPPorts = [ 25 465 587 993 ];
-
-  virtualisation.oci-containers.containers.mailserver = {
-    image = "docker.io/mailserver/docker-mailserver:${version}";
-    hostname = serverName;
-    ports = [
-      "25:25"
-      "465:465"
-      "587:587"
-      "993:993"
-    ];
-    volumes = [
-      "/data/mail/data:/var/mail"
-      "/data/mail/state:/var/mail-state"
-      "/data/mail/logs/:/var/log/mail"
-      "/data/mail/config:/tmp/docker-mailserver"
-      "/etc/localtime:/etc/localtime:ro"
-      "${config.security.acme.certs.${serverName}.directory}:/certs:ro"
-    ];
-    environment = {
-      SSL_TYPE = "manual";
-      SSL_CERT_PATH = "/certs/fullchain.pem";
-      SSL_KEY_PATH = "/certs/key.pem";
-      ENABLE_FAIL2BAN = "1";
-      ENABLE_RSPAMD = "1";
-      ENABLE_CLAMAV = "1";
-      # Rspamd replaces the following
-      ENABLE_POLICYD_SPF = "0";
-      ENABLE_AMAVIS = "0";
-      ENABLE_SPAMASSASSIN = "0";
-      RSPAMD_GREYLISTING = "1";
-      RSPAMD_LEARN = "1";
-      SPAM_SUBJECT = "[SPAM]";
+  virtualisation.oci-containers.containers = {
+    mailserver = {
+      image = "docker.io/mailserver/docker-mailserver:${version}";
+      hostname = serverName;
+      ports = [
+        "25:25" # smtp transfer
+        "465:465" # smtp submission
+        "993:993" # imap retrieval
+      ];
+      volumes = [
+        "/data/mail/data:/var/mail"
+        "/data/mail/state:/var/mail-state"
+        "/data/mail/logs/:/var/log/mail"
+        "/data/mail/config:/tmp/docker-mailserver"
+        "/etc/localtime:/etc/localtime:ro"
+        "${config.security.acme.certs.${serverName}.directory}:/certs:ro"
+      ];
+      environment = {
+        SSL_TYPE = "manual";
+        SSL_CERT_PATH = "/certs/fullchain.pem";
+        SSL_KEY_PATH = "/certs/key.pem";
+        ENABLE_FAIL2BAN = "1";
+        ENABLE_RSPAMD = "1";
+        ENABLE_CLAMAV = "1";
+        # Rspamd replaces the following
+        ENABLE_POLICYD_SPF = "0";
+        ENABLE_AMAVIS = "0";
+        ENABLE_SPAMASSASSIN = "0";
+        RSPAMD_GREYLISTING = "1";
+        RSPAMD_LEARN = "1";
+        SPAM_SUBJECT = "[SPAM]";
+      };
+      extraOptions = [
+        "--cap-add=NET_ADMIN" # for fail2ban
+      ];
     };
-    extraOptions = [
-      "--cap-add=NET_ADMIN" # for fail2ban
-    ];
+
+    webmail = {
+      image = "docker.io/roundcube/roundcubemail:latest";
+      ports = [ "${toString webmailPort}:80" ];
+      environment = {
+        ROUNDCUBEMAIL_DEFAULT_HOST = "ssl://mail.ncoding.at";
+        ROUNDCUBEMAIL_DEFAULT_PORT = "993";
+        ROUNDCUBEMAIL_SMTP_SERVER = "ssl://mail.ncoding.at";
+        ROUNDCUBEMAIL_SMTP_PORT = "465";
+        ROUNDCUBEMAIL_USERNAME_DOMAIN = "ncoding.at";
+      };
+      dependsOn = [ "mailserver" ];
+    };
   };
 
   # Backup to borgbase
