902f51d150c81fd27ef89a8c74a3758bf83b2a77
ch1bo <2621189+ch1bo@users.noreply.github.com>
2025-04-15 19:50:16 +0000

    [create-pull-request] automated change

--- hosts/liskamm/immich.nix
+++ hosts/liskamm/immich.nix
@@ -11,7 +11,7 @@
 let
   # Check release notes
   # https://github.com/immich-app/immich/releases
-  version = "v1.130.3";
+  version = "v1.131.3";
   port = 2283; # not exposed
   networkName = "immich";
   DB_DATABASE_NAME = "immich";
