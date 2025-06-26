fbf758c23d9c062a307ee8ffd2fcd9eff44f541d
Sebastian Nagel <ch1bo@users.noreply.github.com>
2025-06-26 22:32:34 +0200

    Update nextcloud/server (#16)
    Co-authored-by: ch1bo <2621189+ch1bo@users.noreply.github.com>

--- hosts/liskamm/nextcloud.nix
+++ hosts/liskamm/nextcloud.nix
@@ -7,7 +7,7 @@
 let
   # Check release notes
   # https://github.com/nextcloud/server/releases
-  version = "31.0.2";
+  version = "31.0.6";
   port = 8001;
   networkName = "nextcloud";
   serverName = "nextcloud.ncoding.at";
