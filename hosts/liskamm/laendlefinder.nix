972ae4750d0b2553150617bfa43632bb494d55a7
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-08-04 07:37:59 +0200

    Add basic auth to laendlefinder
    The password being cleartext in git and the nix store is not an issue as
    nothing secret is hosted on this service, but access must be non-public.

--- hosts/liskamm/laendlefinder.nix
+++ hosts/liskamm/laendlefinder.nix
@@ -1,11 +1,13 @@
 { config, pkgs, pkgs-unstable, lib, system, inputs, ... }:
 
 {
-  services.nginx.virtualHosts."laendlefinder.ncoding.at" =
-    {
-      forceSSL = true;
-      enableACME = true;
-      root = inputs.laendlefinder.packages.${system}.default;
-      locations."/properties.csv".root = "/data/laendlefinder";
-    };
+  services.nginx.virtualHosts."laendlefinder.ncoding.at" = {
+    forceSSL = true;
+    enableACME = true;
+    root = inputs.laendlefinder.packages.${system}.default;
+    locations."/properties.csv".root = "/data/laendlefinder";
+    # NOTE: Deliberately clear text as a very basic access protection is enough
+    # to claim this to be non-public
+    basicAuth = { user = "fuchsbau"; };
+  };
 }
