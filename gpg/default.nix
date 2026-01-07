a3796bae7e8f74843d53282b4447d8e8392eb7b8
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-01-07 22:14:55 +0100

    Avoid explicit export of SSH_AUTH_SOCK
    This does not work well with ssh agent forwarding

--- gpg/default.nix
+++ gpg/default.nix
@@ -2,33 +2,16 @@
 
 {
   programs.gpg.enable = true;
+
   # Hardened configuration from github.com/drduh/config
   home.file."${config.home.homeDirectory}/.gnupg/gpg.conf".source = ./gpg.conf;
 
-  # TODO: GPG agent replicates ssh keys from keepassxc into
-  # .gnupg/private-keys-v1.d
   services.gpg-agent = {
     enable = true;
     pinentry.package = pkgs.pinentry-gnome3;
-    # NOTE: This is incompatible with agents = [ "ssh" ] of keychain
     enableSshSupport = true;
+    sshKeys = [ "B73C82125079C8FC7966 6FFA59FAA903C906659A" ];
     # Used for agent forwarding
     enableExtraSocket = true;
   };
-  # Always use gpg-agent as ssh agent
-  home.sessionVariables.SSH_AUTH_SOCK = "/run/user/${config.home.uid or "1000"}/gnupg/S.gpg-agent.ssh";
-
-  # Mainly for mailvelope
-  home.packages = [ pkgs.gpgme ];
-  home.file.".mozilla/native-messaging-hosts/gpgmejson.json".text = ''
-    {
-      "name": "gpgmejson",
-      "description": "Integration with GnuPG",
-      "path": "${pkgs.gpgme.dev}/bin/gpgme-json",
-      "type": "stdio",
-      "allowed_extensions": [
-        "jid1-AQqSMBYb0a8ADg@jetpack"
-      ]
-    }
-  '';
 }
