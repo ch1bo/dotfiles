d2b53000b454151da6776e6f42ec05554cbb65b7
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-02-02 08:51:43 +0100

    [weisshorn] Rename zfs pool

--- hosts/weisshorn/zfs.nix
+++ hosts/weisshorn/zfs.nix
@@ -7,10 +7,10 @@
   boot.kernelParams = [ "zfs.zfs_arc_max=12884901888" ];
 
   # Import data even though it's not declaratively mounted by nix
-  boot.zfs.extraPools = [ "data" ];
+  boot.zfs.extraPools = [ "dpool" ];
 
   # Auto-snapshot all marked datasets, e.g
-  # zfs set com.sun:auto-snapshot=true data
+  # zfs set com.sun:auto-snapshot=true dpool
   services.zfs.autoSnapshot.enable = true;
   services.zfs.autoSnapshot.flags = "-k -p --utc";
 }
