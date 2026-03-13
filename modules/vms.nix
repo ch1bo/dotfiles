b42dfcce31cf6594cbf281d225331253dcd11a9a
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-03-13 21:34:25 +0100

    Switch to virt-manager

--- modules/vms.nix
+++ modules/vms.nix
@@ -1,14 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+
+    # TPM for windows 11
+    qemu.swtpm.enable = true;
+  };
+
+  virtualisation.spiceUSBRedirection.enable = true;
 
   # Allow VM management
   users.groups.libvirtd.members = [ config.user.name ];
   users.groups.kvm.members = [ config.user.name ];
 
-  environment.systemPackages = with pkgs; [
-    gnome-boxes # VM frontend
-    dnsmasq # VM networking
-  ];
+  # Frontend
+  programs.virt-manager.enable = true;
+
+  # Filesystem sharing
+  environment.systemPackages = [ pkgs.virtiofsd ];
 }
