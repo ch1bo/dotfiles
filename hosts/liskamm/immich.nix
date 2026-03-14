24047201de559080ac62b4e7389b1c940a8ad876
ch1bo <2621189+ch1bo@users.noreply.github.com>
2026-03-14 01:25:13 +0000

    Update immich-app/immich

--- hosts/liskamm/immich.nix
+++ hosts/liskamm/immich.nix
@@ -11,7 +11,7 @@
 let
   # Check release notes
   # https://github.com/immich-app/immich/releases
-  version = "v2.5.2";
+  version = "v2.5.6";
   port = 2283; # not exposed
   networkName = "immich";
   DB_DATABASE_NAME = "immich";
